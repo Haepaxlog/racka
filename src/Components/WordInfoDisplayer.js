bceda997dedb98c00da979c884b2eaa5d2d1d6d1
Constantin Rohde <rohdeconstantin@gmail.com>
2023-06-09 10:04:21 +0200

    cache words

--- src/Components/WordInfoDisplayer.js
+++ src/Components/WordInfoDisplayer.js
@@ -9,39 +9,45 @@ export const WordInfoDisplayer = (props) => {
     const [isClicked, setClicked] = useState(false)
 
     useEffect(() => {
-        searchWord(props.word, props.lang).then((res) => {
-            data.value = res
-        })
+        if (localStorage.getItem(props.word) === null) {
+            searchWord(props.word, props.lang).then((res) => {
+                data.value = res
+                localStorage.setItem(props.word, JSON.stringify(res))
+            })
+        } else {
+            data.value = JSON.parse(localStorage.getItem(props.word))
+        }
     }, [])
 
     return isClicked
-    ? (
-        <Fragment>
-        <div onclick={() => setClicked(false)} className="absolute w-6 h-6 bg-white">
-            <span>{data.value["word"]}</span>
-            <span>{data.value["lang"]}</span>
-            <div>
-           {
-                data.value["info"].map((info) => {
-                    return (<>
-                    <span>{info["pos"]}</span>
-                    <div>{info["meaning"].map((meaning) => {
-                        return (<>
-                            <span>{meaning}</span>
-                        </>)
-                    })}</div>
-                    </>)
-                })
+        ? (
+            <Fragment>
+                <div onclick={() => setClicked(false)} className="absolute w-6 h-6 bg-white">
+                    <span>{data.value["word"]}</span>
+                    <span>{data.value["lang"]}</span>
+                    <div>
+                        {
+                            data.value["info"].map((info) => {
+                                return (<>
+                                    <span>{info["pos"]}</span>
+                                    <div>{info["meaning"].map((meaning) => {
+                                        return (<>
+                                            <span>{meaning}</span>
+                                        </>)
+                                    })}</div>
+                                </>)
+                            })
+                        }
+                    </div>
+                </div>
+            </Fragment>
+        )
+        : (<div onclick={() => {
+            if (data.value != null) {
+                setClicked(true)
             }
-        </div>
-    </div>
-    </Fragment>
-    ) 
-    : (<div onclick={() => {
-        if (data.value != null) {
-            setClicked(true)
-        }}}>
-        <p>{props.word}</p>
+        }}>
+            <p>{props.word}</p>
         </div>)
 }
 
